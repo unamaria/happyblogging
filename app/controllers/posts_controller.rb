071f733b597783b9a8c6da6489f8c00c89a14192
Maria <unamaria@gmail.com>
2015-07-24 18:40:01 +0200

    Create and show posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.joins(:author).where(users: {handle: params[:handle]})
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		post = Post.new(post_params)
+		if post.save
+			handle = params[:handle]
+			redirect_to post_path(handle, post.id)
+		else
+			render :new
+		end
+	end
+
+	def show
+		@post = Post.find(params[:post_id])
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+end
