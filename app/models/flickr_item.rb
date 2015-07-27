566a64f269cac39e1212740bcdfc7f807c7cabfc
Maria <unamaria@gmail.com>
2015-07-27 19:39:29 +0200

    Create model and view, harcode in render partial to check if it appears in index (it does)

--- app/models/flickr_item.rb
+++ app/models/flickr_item.rb
@@ -1,4 +1,12 @@
 class FlickrItem < ActiveRecord::Base
 	has_one :blog_item, as: :item
 	#has_one :author, class_name: 'User', foreign_key: :user_id, as: :item, through: :blog_item
+
+	def self.url
+
+	end
+
+	def flickr_id
+		
+	end
 end
