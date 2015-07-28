5e7526e3364edbab587e6b09af89bdfde1226298
Maria <unamaria@gmail.com>
2015-07-28 14:46:23 +0200

    getPhotos and getPhotoInfo methods (check in console)

--- app/services/flickr_content_service.rb
+++ app/services/flickr_content_service.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 class FlickrContentService
 
 	def initialize
@@ -7,6 +5,7 @@ class FlickrContentService
 		@root = 'https://api.flickr.com'
 		@api_path = '/services/rest/?'
 		@format = 'json'
+		@user_id = ''
 		@conn = Faraday.new(:url => @root) do |faraday|
 		  faraday.request  :url_encoded             # form-encode POST params
 		  faraday.response :logger                  # log requests to STDOUT
@@ -14,13 +13,30 @@ class FlickrContentService
 		end
 	end
 
-
 	def flickrID(username)
 		method = 'flickr.people.findByUsername'
 		url = "#{@api_path}method=#{method}&api_key=#{@api_key}&username=#{username}&format=#{@format}&nojsoncallback=1"
 
 		response = @conn.get url
-		JSON.parse(response.body)
+		data = JSON.parse(response.body)
+		@user_id = data['user']['id']
+	end
+	
+	def getPhotos
+		method = 'flickr.people.getPublicPhotos'
+		url = "#{@api_path}method=#{method}&api_key=#{@api_key}&user_id=#{@user_id}&format=#{@format}&nojsoncallback=1"
 
+		response = @conn.get url
+		data = JSON.parse(response.body)
+		first_photo_id = data['photos']['photo'][0]['id']
+	end
+	
+	def getPhotoInfo
+		method = 'flickr.photos.getInfo'
+		photo_id = getPhotos
+		url = "#{@api_path}method=#{method}&api_key=#{@api_key}&photo_id=#{photo_id}&format=#{@format}&nojsoncallback=1"
+
+		response = @conn.get url
+		data = JSON.parse(response.body)
 	end
 end
