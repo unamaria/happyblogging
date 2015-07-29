d4dbb79affd760ea435ad1deb5d079c1d5a30e3b
Maria <unamaria@gmail.com>
2015-07-29 16:02:18 +0200

    Newers first

--- app/controllers/blog_items_controller.rb
+++ app/controllers/blog_items_controller.rb
@@ -1,6 +1,6 @@
 class BlogItemsController < ApplicationController
 	def index
 		handle = params[:handle]
-		@blogitems = BlogItem.joins(:author).where(users: {handle: handle})
+		@blogitems = BlogItem.joins(:author).where(users: {handle: handle}).order(created_at: :desc)
 	end
 end
