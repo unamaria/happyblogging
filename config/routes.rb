d8390e556efb5614ff0cd179ee7f68dce4ccff48
Maria <unamaria@gmail.com>
2015-07-29 13:24:45 +0200

    Redirect to user index if current user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   get 'users/profile/:handle' => 'users#show', as: 'user'
-  get ':handle' => 'blog_items#index'
+  get ':handle' => 'blog_items#index', as: 'user_index'
 
 
   resources :users, only: [] do
