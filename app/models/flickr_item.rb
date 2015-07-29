70d220d64d09173c32f59c88c0ec05d13d707cea
Maria <unamaria@gmail.com>
2015-07-29 11:18:48 +0200

    Create flickr_url and direct_url methods

--- app/models/flickr_item.rb
+++ app/models/flickr_item.rb
@@ -2,14 +2,11 @@ class FlickrItem < ActiveRecord::Base
 	has_one :blog_item, as: :item
 	#has_one :author, class_name: 'User', foreign_key: :user_id, as: :item, through: :blog_item
 
-	def flickr_url(flickr_id)
-		# https://www.flickr.com/photos/46626331@N07/<%= photo_id %>
+	def flickr_url(nsid, photo_id)
+		"https://www.flickr.com/photos/#{nsid}/#{photo_id}"
 	end
 
-	def direct_url
-		# from getPhoto: {"id":"17178667303","owner":"46626331@N07","secret":"8bf5e0e924","server":"8707","farm":9,"title":"","ispublic":1,"isfriend":0,"isfamily":0}
-		# how does the last value get generated?
-		# https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-		# https://farm9.staticflickr.com/8707/<%= photo_id %>_5908ab7d1d_k.jpg
+	def direct_url(farm_id, server_id, photo_id, secret)
+		"https://farm#{farm_id}.staticflickr.com/#{server_id}/#{photo_id}_#{secret}_z.jpg"
 	end
 end
