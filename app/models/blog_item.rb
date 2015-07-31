705a0a5a0324842ff84964219cea4957a12dec9d
Maria <unamaria@gmail.com>
2015-07-31 22:26:20 +0200

    Remove content method

--- app/models/blog_item.rb
+++ app/models/blog_item.rb
@@ -1,14 +1,4 @@
 class BlogItem < ActiveRecord::Base
 	belongs_to :author, class_name: 'User', foreign_key: :user_id
 	belongs_to :item, polymorphic: true
-
-	def content
-		if item_type == 'PostItem'
-			PostItem.find(item_id)
-		elsif item_type == 'FlickrItem'
-			FlickrItem.find(item_id)
-		elsif item_type == 'MediumItem'
-			MediumItem.find(item_id)	
-		end
-	end
 end
