334449b61e2c98f8790b0c1960a390eaaadbc494
Maria <unamaria@gmail.com>
2015-08-02 09:33:55 +0200

    Custom css with content_for

--- app/controllers/blog_items_controller.rb
+++ app/controllers/blog_items_controller.rb
@@ -1,6 +1,7 @@
 class BlogItemsController < ApplicationController
 	def index
 		handle = params[:handle]
+		@blog_user = User.find_by_handle(handle)
 		@blogitems = BlogItem.joins(:author).where(users: {handle: handle}).order(created_at: :desc)
 	end
 end
