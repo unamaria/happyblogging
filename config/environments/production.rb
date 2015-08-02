df0043be53f979df0c7ef29d7b4c47d771047040
Maria <unamaria@gmail.com>
2015-08-02 20:22:58 +0200

    Serve static files true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,11 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  
-  # Load CSS in Heroku
-  config.serve_static_assets = true
-	config.assets.compile = true
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
