096ad91af54f342d76a204ee3832fd74fb45fd9c
Maria <unamaria@gmail.com>
2015-08-03 10:54:35 +0200

    Back to original state

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module Happyblogging
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # Add subfolders
-    config.assets.paths += ["#{Rails.root}/app/assets/stylesheets/notebook"]
   end
 end
