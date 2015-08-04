6fdb6400804d5b10b1a56ea7d7128a10cbea6c98
Maria <unamaria@gmail.com>
2015-08-04 17:41:06 +0200

    Implement comments in MediumItem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,19 @@
 class CommentsController < ApplicationController
 	def create
-		@post = PostItem.find(params[:post_item_id])
-		@post.blog_item.comments << Comment.create!(author: params[:comment][:author], body: params[:comment][:body])
-		redirect_to user_post_item_path(@post.author.handle, @post), notice: "Comment successfully added."
+		if params[:post_item_id]
+			item = create_comment(PostItem, params[:post_item_id])
+			redirect_to user_post_item_path(item.author.handle, item), notice: "Comment successfully added."
+		elsif params[:medium_item_id]
+			item = create_comment(MediumItem, params[:medium_item_id])
+			redirect_to user_medium_item_path(item.author.handle, item), notice: "Comment successfully added."
+		end
+	end
+
+	private
+
+	def create_comment(model, item_id)
+		item = model.find(item_id)
+		item.blog_item.comments << Comment.create!(author: params[:comment][:author], body: params[:comment][:body])
+		item
 	end
 end
