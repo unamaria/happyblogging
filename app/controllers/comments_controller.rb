bf29d12d9b39a758f2870c21ee7f288f5731d833
Maria <unamaria@gmail.com>
2015-08-04 17:11:02 +0200

    Fix comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 	def create
 		@post = PostItem.find(params[:post_item_id])
-		@post.blog_item.comments << Comment.create(author: params[:author], body: params[:body])
-		redirect_to @post, notice: "Comment successfully added."
+		@post.blog_item.comments << Comment.create!(author: params[:comment][:author], body: params[:comment][:body])
+		redirect_to user_post_item_path(@post.author.handle, @post), notice: "Comment successfully added."
 	end
 end
