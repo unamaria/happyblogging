71835cf81b7968a2b98916742d8c2f6068a60fdf
Maria <unamaria@gmail.com>
2015-08-04 12:26:12 +0200

    Add tag name

--- app/controllers/blog_items_controller.rb
+++ app/controllers/blog_items_controller.rb
@@ -1,9 +1,10 @@
 class BlogItemsController < ApplicationController
 	def index
 		handle = params[:handle]
+		@tag = params[:tag]
 		@blog_user = User.find_by_handle(handle)
 		if params[:tag].present?
-			@blogitems = BlogItem.items_by_handle_and_tag(handle, params[:tag])
+			@blogitems = BlogItem.items_by_handle_and_tag(handle, @tag)
 		else
 			@blogitems = BlogItem.items_by_handle(handle)
 		end
