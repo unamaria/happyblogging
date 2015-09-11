f22988dd96f272727d14a84a8666679d7e5c80e8
Maria <unamaria@gmail.com>
2015-09-10 22:10:08 -0500

    Include fill in as guest

--- app/assets/javascripts/guest.js
+++ app/assets/javascripts/guest.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
 	$(".guest-user").on ("click", function (e) {
 		e.preventDefault();
 
-		$("#user_email").val("rufus@example.com");
-		$("#user_password").val("rrrrrrrr");
+		$("#user_email").val("dude@example.com");
+		$("#user_password").val("thedudeabides");
 	});
 
 	$(".guest-post").on ("click", function (e) {
 		e.preventDefault();
 
-		$("#title").val("Alice in Wonderland");
+		$("#title").val("The Big Lebowski");
 
-		var body = "'Well, then,' the Gryphon went on, 'if you don't know what to uglify is, you ARE a simpleton.'\r\n\r\nAlice did not feel encouraged to ask any more questions about it, so she turned to the Mock Turtle, and said 'What else had you to learn?'\r\n\r\n'Well, there was Mystery,' the Mock Turtle replied, counting off the subjects on his flappers, '—Mystery, ancient and modern, with Seaography: then Drawling—the Drawling-master was an old conger-eel, that used to come once a week: HE taught us Drawling, Stretching, and Fainting in Coils.'\r\n\r\n'What was THAT like?' said Alice.\r\n\r\n'Well, I can't show it you myself,' the Mock Turtle said: 'I'm too stiff. And the Gryphon never learnt it.'\r\n\r\n'Hadn't time,' said the Gryphon: 'I went to the Classics master, though. He was an old crab, HE was.'\r\n\r\n'I never went to him,' the Mock Turtle said with a sigh: 'he taught Laughing and Grief, they used to say.'"
+		var body = "Well, sir, it's this rug I had. It really tied the room together."
 
 		$("#body").val(body);
-		$("#tags").val("books, quotes");
+		$("#tags").val("movies, quotes, the big lebowski");
 	});
 
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,13 @@
-
-# 5.times do 
-# 	u = User.new
-# 	u.handle = Faker::Team.creature
-# 	u.name = Faker::Name.name
-# 	u.email = Faker::Internet.email
-# 	u.password = 'testtest'
-# 	u.save
-
-	# 3.times do
-	# 	u.posts << Post.new(title: Faker::Lorem.sentence, 
-	# 		body: Faker::Lorem.paragraph(7, false, 4))
-	# end
-# end
-
 dude = User.create!(
-	handle: 	'bear',
-	name: 		'Big Lebowski',
+	handle: 	'dude',
+	name: 		'Jeffrey Lebowski',
 	email: 		'dude@example.com',
 	password: 'thedudeabides')
 
-40.times do
-	post = PostItem.new(
-			title: Faker::Lorem.sentence, 
-			body: Faker::Lorem.paragraph(7, false, 4))
-	dude.post_items << post
-	post.blog_item = BlogItem.create!(user_id: dude.id, item_type: 'PostItem')
-end
+# 40.times do
+# 	post = PostItem.new(
+# 			title: Faker::Lorem.sentence, 
+# 			body: Faker::Lorem.paragraph(7, false, 4))
+# 	dude.post_items << post
+# 	post.blog_item = BlogItem.create!(user_id: dude.id, item_type: 'PostItem')
+# end
