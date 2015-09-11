4283b8647e987c095dea8dd9ef8b6f1bd7a21b70
Maria <unamaria@gmail.com>
2015-09-10 21:04:50 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,17 @@
 	# 		body: Faker::Lorem.paragraph(7, false, 4))
 	# end
 # end
+
+dude = User.create!(
+	handle: 	'bear',
+	name: 		'Big Lebowski',
+	email: 		'dude@example.com',
+	password: 'thedudeabides')
+
+40.times do
+	post = PostItem.new(
+			title: Faker::Lorem.sentence, 
+			body: Faker::Lorem.paragraph(7, false, 4))
+	dude.post_items << post
+	post.blog_item = BlogItem.create!(user_id: dude.id, item_type: 'PostItem')
+end
