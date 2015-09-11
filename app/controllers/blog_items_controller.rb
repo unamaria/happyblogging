c1aa8a39daac966f585e14ebf7c04b2dd8e9ebcd
Maria <unamaria@gmail.com>
2015-09-10 21:31:01 -0500

    Add pagination

--- app/controllers/blog_items_controller.rb
+++ app/controllers/blog_items_controller.rb
@@ -4,9 +4,11 @@ class BlogItemsController < ApplicationController
 		@tag = params[:tag]
 		@blog_user = User.find_by_handle(handle)
 		if params[:tag].present?
-			@blogitems = BlogItem.items_by_handle_and_tag(handle, @tag)
+			@blogitems = BlogItem.items_by_handle_and_tag(handle, @tag).paginate(
+				:page => params[:page], :per_page => 10)
 		else
-			@blogitems = BlogItem.items_by_handle(handle)
+			@blogitems = BlogItem.items_by_handle(handle).paginate(
+				:page => params[:page], :per_page => 10)
 		end
 	end
 end
